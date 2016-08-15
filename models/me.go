2315abcf36d0500989e2b68024fd519d2e7a9397
John Nguyen <jnguyen@thefrontiergroup.com.au>
2016-08-15 09:16:45 +0800

    adds example model

---
+++ models/me.go
@@ -0,0 +1,17 @@
+package models
+
+// Me is you
+var Me = Resume{
+	BasicInformation: Basic{
+		Name: "John Nguyen",
+	},
+	WorkExperience:      []Work{},
+	VolunteerExperience: []Volunteer{},
+	EducationHistory:    []Education{},
+	Awards:              []Award{},
+	Publications:        []Publication{},
+	Skills:              []Skill{},
+	Languages:           []Language{},
+	Interests:           []Interest{},
+	References:          []Reference{},
+}
