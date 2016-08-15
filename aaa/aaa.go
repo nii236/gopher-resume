09be515aa03b29d6f69bb282054c08abf44f791d
John Nguyen <jnguyen@thefrontiergroup.com.au>
2016-08-15 09:40:36 +0800

    adds aaa package back in without submodule tracking

---
+++ aaa/aaa.go
@@ -0,0 +1,34 @@
+package aaa
+
+import (
+	"crypto/rand"
+	"math/big"
+	"strings"
+)
+
+// Generate will generate a friendly token of a numAdj
+// adjectives followed by an animal separated by sep
+func Generate(numAdj int, sep string) string {
+	result := []string{}
+	for i := 0; i < numAdj; i++ {
+		result = append(result, randomAdjective())
+	}
+	result = append(result, randomAnimal())
+	return strings.Join(result, sep)
+}
+
+func randomAdjective() string {
+	n, err := rand.Int(rand.Reader, big.NewInt(int64(len(adjectives))))
+	if err != nil {
+		return ""
+	}
+	return strings.ToLower(adjectives[int(n.Int64())-1])
+}
+
+func randomAnimal() string {
+	n, err := rand.Int(rand.Reader, big.NewInt(int64(len(animals))))
+	if err != nil {
+		return ""
+	}
+	return strings.ToLower(animals[int(n.Int64())-1])
+}
